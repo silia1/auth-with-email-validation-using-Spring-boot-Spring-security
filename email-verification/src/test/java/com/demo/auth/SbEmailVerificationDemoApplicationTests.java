9b2932dc06d2100e2903615c3a8c35dab04bda55
silia1 <elghazisilia@gmail.com>
2025-05-08 21:49:23 +0100

    Initial commit

---
+++ email-verification/src/test/java/com/demo/auth/SbEmailVerificationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEmailVerificationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
